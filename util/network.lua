1a0a58fe4f223470088bbbff26a73db39c7702eb
gdanko <gdanko@protonmail.com>
2025-11-30 11:10:10 -0800

    add Linux network functions

--- util/network.lua
+++ util/network.lua
@@ -27,6 +27,22 @@ function network_data_linux(interface)
     return nil, nil
 end
 
+function darwin_interface_exists(interface)
+    local success, stdout, _ = wezterm.run_child_process({ "networksetup", "-listallhardwareports" })
+    if success then
+        for port, device, _ in stdout:gmatch("Hardware Port: ([^\n]+)\nDevice: ([^\n]+)\nEthernet Address: ([^\n]+)") do
+            if device == interface then
+                if string.find(port, "Wi-Fi", 0, true) then
+                    return "wireless", true
+                else
+                    return "wired", true
+                end
+            end
+        end
+    end
+    return nil, false
+end
+
 function darwin_is_connected(interface)
     local success, stdout, _ = wezterm.run_child_process({ "ifconfig", interface })
     if success then
@@ -45,27 +61,36 @@ function darwin_is_connected(interface)
 end
 
 function darwin_get_icon(interface)
-    local success, stdout, _ = wezterm.run_child_process({ "networksetup", "-listallhardwareports" })
-    if success then
-        for port, device, address in stdout:gmatch("Hardware Port: ([^\n]+)\nDevice: ([^\n]+)\nEthernet Address: ([^\n]+)") do
-            if device == interface then
-                if string.find(port, "Wi-Fi", 0, true) then
-                    if darwin_is_connected(interface) then
-                        return wezterm.nerdfonts.md_wifi_strength_4
-                    else
-                        return wezterm.nerdfonts.md_wifi_strength_off
-                    end
-                else
-                    if darwin_is_connected(interface) then
-                        return wezterm.nerdfonts.md_network
-                    else
-                        return wezterm.nerdfonts.md_network_off
-                    end
-                end
+    type, exists = darwin_interface_exists(interface)
+    if exists then
+        if type == "wireless" then
+            if darwin_is_connected(interface) then
+                return wezterm.nerdfonts.md_wifi_strength_4
+            else
+                return wezterm.nerdfonts.md_wifi_strength_off
+            end
+        else
+            if darwin_is_connected(interface) then
+                return wezterm.nerdfonts.md_network
+            else
+                return wezterm.nerdfonts.md_network_off
             end
         end
     end
     return wezterm.nerdfonts.md_network
+end
+
+function linux_interface_exists(interface)
+    filename = path_join({ "/sys/class/net", interface })
+    if is_dir(filename) then
+        filename = path_join({ "/sys/class/net", interface, "wireless" })
+        if is_dir(filename) then
+            return "wireless", true
+        else
+            return "wired", true
+        end
+    end
+    return nil, false
 end
 
 function linux_is_connected(interface)
@@ -76,21 +101,27 @@ function linux_is_connected(interface)
     return wezterm.nerdfonts.md_network
 end
 
-function linux_interface_exists(interface)
-    filename = path_join({ "/sys/class/net", interface })
-    if is_dir(filename) then
-        return true
-    end
-    return false
-end
-
 function linux_get_icon(interface)
-    if not linux_interface_exists(interface) then
-        return wezterm.nerdfonts.md_alert
+    type, exists = linux_interface_exists(interface)
+    if exists then
+        if type == "wireless" then
+            if linux_is_connected(interface) then
+                return wezterm.nerdfonts.md_wifi_strength_4
+            else
+                return wezterm.nerdfonts.md_wifi_strength_off
+            end
+        else
+            if linux_is_connected(interface) then
+                return wezterm.nerdfonts.md_network
+            else
+                return wezterm.nerdfonts.md_network_off
+            end
+        end
     end
+    return wezterm.nerdfonts.md_network
 end
 
-function get_interface_type(config, interface)
+function get_interface_icon(config, interface)
     if config.os_name == "darwin" then
         return darwin_get_icon(interface)
     elseif config.os_name == "linux" then
@@ -98,7 +129,11 @@ function get_interface_type(config, interface)
     end
 end
 
-network.get_interface_type = get_interface_type
+network.darwin_interface_exists = darwin_interface_exists
+network.darwin_is_connected = darwin_is_connected
+network.get_interface_icon = get_interface_icon
+network.linux_interface_exists = linux_interface_exists
+network.linux_is_connected = linux_is_connected
 network.network_data_darwin = network_data_darwin
 network.network_data_linux = network_data_linux
 
