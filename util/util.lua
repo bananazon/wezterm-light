eef435aaadeb46448177f7940f96db61de5ecbca
gdanko <gdanko@protonmail.com>
2025-12-02 12:45:22 -0800

    Changes in some of the other util modules

--- util/util.lua
+++ util/util.lua
@@ -23,6 +23,7 @@ util.file_exists = filesystem.file_exists
 util.get_cwd = filesystem.get_cwd
 util.is_dir = filesystem.is_dir
 util.path_join = filesystem.path_join
+util.which = filesystem.which
 
 local json = require(folderOfThisFile .. "json")
 util.json_parse_file = json.json_parse_file
@@ -30,12 +31,11 @@ util.json_parse_string = json.json_parse_string
 
 local network = require(folderOfThisFile .. "network")
 util.darwin_interface_exists = network.darwin_interface_exists
+util.darwin_interface_icon = network.darwin_interface_icon
 util.darwin_is_connected = network.darwin_is_connected
-util.get_interface_icon = network.get_interface_icon
 util.linux_interface_exists = network.linux_interface_exists
+util.linux_interface_icon = network.linux_interface_icon
 util.linux_is_connected = network.linux_is_connected
-util.network_data_darwin = network.network_data_darwin
-util.network_data_linux = network.network_data_linux
 
 local strings = require(folderOfThisFile .. "strings")
 util.get_plural = strings.get_plural
@@ -43,5 +43,8 @@ util.pad_string = strings.pad_string
 util.split_lines = strings.split_lines
 util.split_words = strings.split_words
 util.string_split = strings.string_split
+
+local time = require(folderOfThisFile .. "time")
+util.get_clock_icon = time.get_clock_icon
 
 return util
