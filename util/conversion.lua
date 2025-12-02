d62181780cd7c9c17d51c951849bf47d697e295b
gdanko <gdanko@protonmail.com>
2025-12-02 12:47:03 -0800

    Add auto to the byte converter

--- util/conversion.lua
+++ util/conversion.lua
@@ -1,37 +1,60 @@
 local wezterm = require "wezterm"
 local conversion = {}
 
+function pad_float(number)
+    return string.format("%.2f", number)
+end
+
 -- Simple byte converter
 -- Usage: foo = util.byte_converter(67108864, "Gi")
 --        foo = util.byte_converter(67108864, "M")
-function byte_converter(bytes, unit)
-    local divisor = 0
-    local multiple = 0
+function byte_converter(number, unit)
+    if unit == nil then
+        unit = "auto"
+    end
     local suffix = "B"
-    local prefix = unit:sub(1, 1)
-    if unit:find("i" .. "$") then
-        divisor = 1024
-    else
-        divisor = 1000
-    end
 
-    if prefix == "K" then
-        multiple = 1
-    elseif prefix == "M" then
-        multiple = 2
-    elseif prefix == "G" then
-        multiple = 3
-    elseif prefix == "T" then
-        multiple = 4
-    elseif prefix == "P" then
-        multiple = 5
-    elseif prefix == "E" then
-        multiple = 6
+    if unit == "auto" then
+        for _, unit_prefix in ipairs { "", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi" } do
+            if math.abs(number) < 1024.0 then
+                return string.format("%.2f %s%s", number, unit_prefix, suffix)
+            end
+            number = number / 1024
+        end
+        return string.format("%.2f Yi%s", number, suffix)
     else
-        return string.format("%.2f %s", bytes, suffix)
-    end
+        local divisor = 1000
+        if string.len(unit) == 2 and ends_with(unit, "i") then
+            divisor = 1024
+        end
 
-    return string.format("%.2f %s%s", bytes / divisor ^ multiple, unit, suffix)
+        prefix_map = {
+            K = 1,
+            Ki = 1,
+            M = 2,
+            Mi = 2,
+            G = 3,
+            Gi = 3,
+            T = 4,
+            Ti = 4,
+            P = 5,
+            Pi = 5,
+            E = 6,
+            Ei = 6,
+            Z = 7,
+            Zi = 7,
+            Y = 8,
+            Yi = 8,
+        }
+
+        if prefix_map[unit] ~= nil then
+            power = prefix_map[unit]
+            value = number / (divisor ^ power)
+            return string.format("%.2f %s%s", value, unit, suffix)
+        else
+            return string.format("%s %s", number, suffix)
+        end
+    end
 end
 
 function duration(delta)
